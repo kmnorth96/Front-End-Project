e2d48874d936642cfb25f5221744e3a4a11dc37b
kmnorth96 <kmnorth96@gmail.com>
2022-11-23 11:13:59 -0800

    working city search bar

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const searchButton = document.querySelector("#search");
 //Weather Info Div
 const weatherInfo = document.querySelector(".Weather-Info");
 
+//City Search
+const citySearch = document.querySelector('#city');
+
 
 function weatherReport(_displayTemp) {
     const div = document.createElement("div");
@@ -20,20 +23,63 @@ function weatherReport(_displayTemp) {
 
 
 //Render Weather Info Base on Search
-searchButton.addEventListener("click",() => {
-    const latitude = latitudeId.value;
-    const longitude = longitudeId.value;
+// searchButton.addEventListener("click",() => {
+//     const latitude = latitudeId.value;
+//     const longitude = longitudeId.value;
 
-    weatherInfo.innerHTML = "";
+//     weatherInfo.innerHTML = "";
+//     $.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&models=best_match&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&timezone=America%2FLos_Angeles`
+//     , function(data){
+//         console.log(data);
 
-    $.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`, function(data){
-        console.log(data)
+//             const temperature = data.hourly.temperature_2m[0];
 
-        for (let _key in data) {
-            const temperature = `${data.hourly.temperature_2m[1]}`;
+//             weatherReport(temperature);
+//             console.log(temperature);
+//     });
+// });
 
-            weatherReport(temperature);
-            console.log(temperature);
-        }
+
+searchButton.addEventListener("click", () => {
+    const city = citySearch.value;
+    const latitude = ''
+    const longitude = ''
+    weatherInfo.innerHTML = '';
+    $.get(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`, function(data){
+       
+        const latitude = data.results[0].latitude;
+        const longitude = data.results[0].longitude;
+        console.log('firt get', data);
+        whatever(latitude, longitude);
+        // console.log('latitude',typeof latitude);
+        // console.log('longitude',typeof longitude);
+
     });
-})
+    console.log('latitude',typeof latitude);
+    console.log('longitude',typeof longitude);
+    // const latitude = data.results[0].latitude;
+    // const longitude = data.results[0].longitude;
+
+
+    // $.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`, function(tempData){
+    //     console.log(tempData);
+
+    //         const temperature = tempData.hourly.temperature_2m[0];
+
+    //         weatherReport(temperature);
+    //         console.log(temperature);
+    // });
+
+});
+      function whatever(lat, long){ 
+        $.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m`, function(tempData){
+            console.log(tempData);
+    
+                const temperature = tempData.hourly.temperature_2m[0];
+
+                console.log(temperature);
+
+                weatherReport(temperature);
+              
+        });
+    }
